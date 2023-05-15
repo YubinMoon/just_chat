7950e54879aa5f2ff1b54868fb40ee36722a8492
terry <munu220@gmail.com>
2023-05-15 02:00:42 +0000

    작업물 저장

---
+++ frontend/src/component/SVG.jsx
@@ -0,0 +1,17 @@
+export const RightConsoleTransparent = () => {
+    return (
+        <svg id="b" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.39 175.39"> <g id="c"> <path d="m155.89,20.84l-18.01-18.01L2.83,137.89l18.01,18.01c22.51,22.51,49.52,22.51,67.53,4.5l72.03-72.03c18.01-18.01,18.01-45.02-4.5-67.53Z" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <circle cx="115.87" cy="78.59" r="6" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <circle cx="115.87" cy="58.59" r="6" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <circle cx="135.87" cy="78.59" r="6" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <circle cx="135.87" cy="58.59" r="6" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <path d="m104.39,106.07c0,8.84-7.16,16-16,16-8.84,0-16-7.16-16-16,0-8.84,7.16-16,16-16,8.84,0,16,7.16,16,16Z" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <polygon points="138.59 27.58 135.76 24.75 138.59 21.92 137.18 20.51 134.35 23.33 131.52 20.51 130.11 21.92 132.94 24.75 130.11 27.58 131.52 28.99 134.35 26.16 137.18 28.99 138.59 27.58" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> <circle cx="50.2" cy="119.5" r="4.5" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="4" /> </g> </svg>
+    )
+}
+
+export const LeftConsoleTransparent = () => {
+    return (
+        <svg id="b" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.39 175.39"> <g id="c"> <path d="m155.89,20.84l-18.01-18.01L2.83,137.89l18.01,18.01c22.51,22.51,49.52,22.51,67.53,4.5l72.03-72.03c18.01-18.01,18.01-45.02-4.5-67.53Z" fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <circle cx="115.87" cy="78.59" r={6} fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <circle cx="115.87" cy="58.59" r={6} fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <circle cx="135.87" cy="78.59" r={6} fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <circle cx="135.87" cy="58.59" r={6} fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <path d="m104.39,106.07c0,8.84-7.16,16-16,16-8.84,0-16-7.16-16-16,0-8.84,7.16-16,16-16,8.84,0,16,7.16,16,16Z" fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <polygon points="138.59 27.58 135.76 24.75 138.59 21.92 137.18 20.51 134.35 23.33 131.52 20.51 130.11 21.92 132.94 24.75 130.11 27.58 131.52 28.99 134.35 26.16 137.18 28.99 138.59 27.58" fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> <circle cx="50.2" cy="119.5" r="4.5" fill="none" stroke="#000" strokeMiterlimit={10} strokeWidth={4} /> </g> </svg>
+    )
+}
+
+export const Cross = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" > <line x1={50} y1={0} x2={50} y2={100} strokeWidth={6} /> <line x1={0} y1={50} x2={100} y2={50} strokeWidth={6} /> </svg>
+    )
+}
--- frontend/src/component/ServerPannel.js
+++ frontend/src/component/ServerPannel.js
@@ -1,42 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import fastapi from '../lib/api'
-import styles from './ServerPannel.module.css'
 import { Outlet, useNavigate, useRoutes, useParams } from 'react-router-dom';
 import useStore from '../lib/store';
+import { RightConsoleTransparent, Cross } from './SVG';
 
-function ServerBox({ children }) {
+const ServerCreate = ({ }) => {
     return (
-        <div className={`${styles.itemBox}`}>
-            {children}
+        <div className="flex relative mb-2 md:hover:bg-white">
+            <div className={"rounded-3xl transition-all hover:rounded-2xl hover:bg-violet-800 flex m-auto justify-center items-center w-14 h-14 bg-neutral-700 mx-1.5 group"}>
+                <div className="relative w-7 h-7 stroke-green-600 transition-all group-hover:stroke-white">
+                    <Cross />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ServerBox = ({ children, selected, onClick, backImage }) => {
+    return (
+        <div className="flex relative mb-2">
+            <div className={(selected ? "flex" : "hidden") + ' flex-col absolute h-full justify-center'}>
+                <div className='bg-white w-1 h-10 rounded-r-sm' />
+            </div>
+            <div className={(selected ? "rounded-2xl" : "rounded-3xl transition-all hover:rounded-2xl hover:bg-emerald-700") + " flex m-auto justify-center items-center w-14 h-14 bg-neutral-700 mx-1.5"} onClick={onClick}>
+                {children}
+            </div>
         </div>
     )
 }
 
 export default function ServerList() {
     const { serverList } = useStore(state => state)
+    const { server, channel } = useParams()
     const navigate = useNavigate()
+
     return (
         <>
-            <div className={styles.serverlist}>
-                <ServerBox>
-                    <div className={`${styles.btn}`} onClick={() => {
-                        navigate("/")
-                    }}>
-                        메인
+            <div className={"bg-neutral-900 overflow-scroll scrollbar-none pt-3"}>
+                <ServerBox selected={!server} onClick={() => { navigate("/") }}>
+                    <div className="relative w-9 h-9">
+                        <RightConsoleTransparent />
                     </div>
                 </ServerBox>
-                <ServerBox />
+                <div className='flex justify-center pb-2'>
+                    <div className='bg-neutral-500 w-10 h-px'></div>
+                </div>
                 {serverList.map(e => (
-                    <ServerBox key={e.id} name={e.name}>
-                        <div>
-                            {e && <div className={styles.btn} onClick={() => {
-                                navigate("/" + e.id)
-                            }}>
-                                {e.name[0]}
-                            </div>}
-                        </div>
+                    <ServerBox selected={e.id == server} onClick={() => { navigate("/" + e.id) }} key={e.id}>
+                        {e && <div className='text-2xl text-white items-center flex justify-center'>
+                            <span>
+                                {e.name.toUpperCase()[0]}
+                            </span>
+                        </div>}
                     </ServerBox>
                 ))}
+                <ServerCreate />
             </div>
         </>
     )
